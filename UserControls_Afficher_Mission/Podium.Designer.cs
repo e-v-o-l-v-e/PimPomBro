6baaddb24690d9051ab5476e2c7d00f573d93d57
apt-install-fella <fellabennounas@gmail.com>
2025-05-28 12:05:03 +0200

    Add files via upload

---
+++ UserControls_Afficher_Mission/Podium.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace AfficheMisssions
+{
+    partial class Podium
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Arial", 15.75F);
+            this.lbl1.Location = new System.Drawing.Point(107, 0);
+            this.lbl1.MaximumSize = new System.Drawing.Size(90, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(65, 24);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "label1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.BackColor = System.Drawing.Color.Transparent;
+            this.lbl2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(7, 41);
+            this.lbl2.MaximumSize = new System.Drawing.Size(70, 0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(65, 24);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "label2";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Arial", 15.75F);
+            this.lbl3.Location = new System.Drawing.Point(206, 42);
+            this.lbl3.MaximumSize = new System.Drawing.Size(90, 0);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(65, 24);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "label3";
+            // 
+            // Podium
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.BackgroundImage = global::AfficheMisssions.Properties.Resources.podiumBG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Podium";
+            this.Size = new System.Drawing.Size(303, 211);
+            this.Load += new System.EventHandler(this.Podium_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+    }
+}
---
+++ UserControls_Afficher_Mission/Podium.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AfficheMisssions
+{
+    public partial class Podium : UserControl
+    {
+        public Podium()
+        {
+            InitializeComponent();
+        }
+
+        public Podium(String premier, String deuxieme, String troisieme)
+        {
+            InitializeComponent();
+            lbl1.Text = premier;
+            lbl2.Text = deuxieme;   
+            lbl3.Text = troisieme;
+        }
+
+        private void Podium_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
